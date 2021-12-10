8fb18d69b6991f39d4ebc787ac3255dccc16d41a
Opeyemi Ogundeyi <67125759+YemiOG@users.noreply.github.com>
2021-12-10 02:13:10 +0100

    Initial Commit

---
+++ auth/api-auth-with-jwt/README.md
@@ -0,0 +1,2 @@
+# api-auth-with-jwt
+The Authentication API enables you to manage all aspects of user identity. It enables users to register and log in. JWT Authentication is used to verify the identity of a user. In other words, it proves that the clients trying to access a remote server are really who they say they are.
---
+++ auth/model/User.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        min: 6,
+        max: 255
+    },
+    email: {
+        type: String,
+        required: true,
+        min: 6,
+        max: 255
+    },
+    password: {
+        type: String,
+        required: true,
+        min: 6,
+        max: 2048
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+
+    
+});
+
+export default mongoose.model('User,', userSchema);
